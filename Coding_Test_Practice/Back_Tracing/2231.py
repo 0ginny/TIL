54a2447ca433db223680945bd081b94389ad3abe
Jinny Kim <kyj20214@gmail.com>
2024-03-19 14:25:19 +0900

    2231 수정

--- Coding_Test_Practice/Back_Tracing/2231.py
+++ Coding_Test_Practice/Back_Tracing/2231.py
@@ -3,7 +3,7 @@
 - 생성자의 조건
   - 목표 자연수보다 작음
   - 자신 + 각 자리 숫자의 합(%10) = 목표 자연수
-    - 각자리 숫자의 합 < 10 * 자리수(//10)
+    - 각자리 숫자의 합 < 10 * 자리수
       - 아무리 최대에서도 60번만 반복하면 됨. 가능
 
 논리
@@ -14,32 +14,44 @@
 
 t = int(input())
 
-def tens(n):
-  num = 0
-  while(n<0)
-    num += 1
-    n = int(n/10)
-  return num
 
-def gen(n): # 생성 함수
-  if n <10:
-    return 2 * n
-  else :
-    total = n
-    for _ in range(tens(n)+1):
-      total += n % 10
-      n //= 10
-    return total
+def tens(n): # 자릿수 구하는 함수
+    num = 1
+    while (n >= 10) :
+        num += 1
+        n = int(n / 10)
+    return num
+
+
+def gen(n):  # 생성 함수
+    if n < 10:
+        return 2 * n
+    else:
+        total = n
+        for _ in range(tens(n) + 1):
+            total += n % 10
+            n //= 10
+        return total
+
 
 ans = 0
-if t >= 10 : # 생성자 가능
-  d = tens(t)
-  repeat = 10*d -1
-  print(repeat)
+if t >= 10:  # 생성자 가능
+    d = tens(t)
+    repeat = 10 * d
 
-  for i in range(1,repeat):
-    if t == gen(n-i):
-      ans = n-i
-      break
+    for i in range(repeat):
+        new = t - repeat + i
+        if t == gen(new):
+            ans = new
+            break
+        print(f'new : {new}, gen : {gen(new)}')
+else :
+    for i in range(1,t):
+        if 2 * i == t:
+            ans = i
 print(ans)
-    
+
+
+"""
+t < 10 일 경우를 고려하지 않았음.
+"""
