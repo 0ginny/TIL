a09dc30b2ee97b00f813f45ff314c1d6ccf2af03
Ginny <kyj20214@gmail.com>
2024-05-18 22:57:08 +0900

    day19 hw_turtle_race_mycode add print winner

--- 100Days_of_Python_start_240515/day19_EventListener/3_turtle_race_game.py
+++ 100Days_of_Python_start_240515/day19_EventListener/3_turtle_race_game.py
@@ -6,6 +6,7 @@ color_names = ['yellow', "gold", 'orange', 'red', 'maroon', 'violet', 'magenta',
                "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray", "white"]
 
 screen = Screen()
+screen.setup(500, 400)
 #
 # bet = turtle.textinput("Make your bet", "who will win the race? Enter a color")
 
@@ -17,18 +18,26 @@ def init_turtle(tim, idx ):
     tim.up()
 
 
-start_x = -300
-start_y = -200
+start_x = -230
+start_y = -180
 
+end = False
 class Turtle_new(turtle.Turtle):
     def start(self,idx):
+        self.color_name = color_names[idx]
         self.shape('turtle')
-        self.color(color_names[idx])
+        self.color(self.color_name)
         self.penup()
         self.goto(start_x,start_y+ 50 * idx)
 
     def move(self):
         self.fd(rd.randint(1,21))
+
+    def check_win(self):
+        if self.pos()[0] > 190:
+            screen.bye()
+            print(f'{self.color_name} win!')
+
 #
 
 t1 = Turtle_new()
@@ -39,10 +48,15 @@ t1.start(1)
 t2.start(2)
 t3.start(3)
 
-while 1:
+
+while 1 :
     t1.move()
     t2.move()
     t3.move()
+
+    t1.check_win()
+    t2.check_win()
+    t3.check_win()
 
 
 screen.exitonclick()
