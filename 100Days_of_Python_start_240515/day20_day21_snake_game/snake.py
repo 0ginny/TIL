6b38cfb51f742d69a47b5e817773d0e1d01d016d
Ginny <kyj20214@gmail.com>
2024-05-19 21:07:50 +0900

    snake class soft coding

--- 100Days_of_Python_start_240515/day20_day21_snake_game/snake.py
+++ 100Days_of_Python_start_240515/day20_day21_snake_game/snake.py
@@ -1,9 +1,12 @@
 from turtle import Turtle
 
 start_len = 3
-
+move_distance = 20
+unit_size = 20
 start_x = 0
 start_y = 0
+
+
 class Snake():
     def __init__(self):
         self.body = []
@@ -15,8 +18,9 @@ class Snake():
             tim.color('white')
             # tim.speed(6) # normal speed || range 0.5 ~ 10
             tim.penup()
-            tim.goto(start_x - 20 * idx, start_y)
+            tim.goto(start_x - unit_size * idx, start_y)
             self.body.append(tim)
+
     def follow(self):
         # 이전 unit 따라가기
         for idx in range(1, len(self.body)):
@@ -25,4 +29,4 @@ class Snake():
     def forward(self):
         # 앞으로 전진
         self.follow()
-        self.body[0].fd(20)
+        self.body[0].fd(move_distance)
