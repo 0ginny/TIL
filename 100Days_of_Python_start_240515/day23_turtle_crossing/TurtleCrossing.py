100b940dde8bbb4b2941c326a3b1532a55075c50
Jinny Kim <kyj20214@gmail.com>
2024-05-20 16:17:05 +0900

    basic screen

---
+++ 100Days_of_Python_start_240515/day23_turtle_crossing/TurtleCrossing.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+import time
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+# main code
+if __name__ == '__main__':
+    screen = Screen()
+    screen.title("Timmy Crossing Game")
+    screen.setup(width=SCREEN_WIDTH, height= SCREEN_HEIGHT)
+
+
+
+    screen.exitonclick()
---
+++ 100Days_of_Python_start_240515/day23_turtle_crossing/outline.md
@@ -0,0 +1,20 @@
+# 클래스
+- Timmy(turtle)
+- Car
+- ScoreBoard
+
+# 구현 단계
+- 기본 화면 구현
+- timmy 클래스 생성
+- timmy control
+- car 클래스 생성
+- car 램덤 움직임
+- clear 구현
+  - timmy 위치 초기화
+  - car 속도 증가
+- scoreboard 구현
+- game over 구현
+
+추가적인 구현
+
+- 각 자동차끼리 랜덤 속도
