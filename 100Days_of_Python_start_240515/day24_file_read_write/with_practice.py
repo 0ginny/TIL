0b2870d6ad383cb3a0973a8fff619a193ab3d664
Jinny Kim <kyj20214@gmail.com>
2024-05-21 09:10:17 +0900

    open write mode

--- 100Days_of_Python_start_240515/day24_file_read_write/with_practice.py
+++ 100Days_of_Python_start_240515/day24_file_read_write/with_practice.py
@@ -5,8 +5,12 @@
 # file.close()
 
 # with를 써도 좋아. with 밖에선 할당이 끝나는 거야.
-with open('my_file.txt') as file:
-    content = file.read()
-    print(content)
+# with open('my_file.txt') as file:
+#     content = file.read()
+#     print(content)
 # 이후 아래처럼 참조 불가.
 # print(file.read())
+
+# open mode / r : read / w : write / a : add
+with open('my_file.txt', mode= 'a') as file:
+    file.write("\nNew Text")
