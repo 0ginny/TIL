31265e07357334249017f154464722ab13be4f6d
Jinny Kim <kyj20214@gmail.com>
2024-05-21 13:14:02 +0900

    check input state is right

--- 100Days_of_Python_start_240515/day25_pandas/us-states-game-start/us_state_game.py
+++ 100Days_of_Python_start_240515/day25_pandas/us-states-game-start/us_state_game.py
@@ -1,6 +1,15 @@
 import turtle as t
+import pandas as pd
 
-
+# data load
+def get_state_data(csv_path = '50_states.csv'):
+    data = pd.read_csv(csv_path)
+    return data
+# check the state is in 50 states
+def is_in_US_states(state, pd_data):
+    if state in pd_data.state.values:
+        return True
+    return False
 
 if __name__ == '__main__':
     screen = t.Screen()
@@ -9,8 +18,19 @@ if __name__ == '__main__':
     image = 'blank_states_img.gif'
     screen.bgpic(image)
 
+    # input user answer
+    text_input_title = 'Guess the State'
+    text_input_prompt = "what's another state's name? "
+    data = get_state_data()
+    answer_state = screen.textinput(title=text_input_title, prompt=text_input_prompt)
+    if is_in_US_states(answer_state,data) :
+        pass
+    screen.mainloop()
 
-# reference code
+
+
+
+### reference code
 
 ## add mouse click event
 # def mouse_click_pos(x,y):
