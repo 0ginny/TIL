49607535bc7c36340ce35577bcb73eab662c2121
Jinny Kim <kyj20214@gmail.com>
2024-05-21 09:05:39 +0900

    about using with

--- 100Days_of_Python_start_240515/day24_file_read_write/with_practice.py
+++ 100Days_of_Python_start_240515/day24_file_read_write/with_practice.py
@@ -1,5 +1,12 @@
-file = open('my_file.txt')
-content = file.read()
-print(content)
-# 만약 계속 열고 있으면 컴퓨터의 리소스(메모리)가 할당되고 있는 거야.
-file.close()
+# file = open('my_file.txt')
+# content = file.read()
+# print(content)
+# # 만약 계속 열고 있으면 컴퓨터의 리소스(메모리)가 할당되고 있는 거야.
+# file.close()
+
+# with를 써도 좋아. with 밖에선 할당이 끝나는 거야.
+with open('my_file.txt') as file:
+    content = file.read()
+    print(content)
+# 이후 아래처럼 참조 불가.
+# print(file.read())
