04a7f720f87bb63f1db3d0c9aca55133a2695683
Jinny Kim <kyj20214@gmail.com>
2024-05-21 13:25:03 +0900

    operate answering

--- 100Days_of_Python_start_240515/day25_pandas/us-states-game-start/us_state_game.py
+++ 100Days_of_Python_start_240515/day25_pandas/us-states-game-start/us_state_game.py
@@ -1,9 +1,17 @@
 import turtle as t
 import pandas as pd
+title_name = "U.S. States Game"
+text_input_title = 'Guess the State'
+text_input_prompt = "what's another state's name? "
+image_path = 'blank_states_img.gif'
 
+search_col = 'state'
+
+answers = []
 # data load
 def get_state_data(csv_path = '50_states.csv'):
     data = pd.read_csv(csv_path)
+    print(data[data[search_col] == 'Texas'].values[0])
     return data
 # check the state is in 50 states
 def is_in_US_states(state, pd_data):
@@ -11,20 +19,32 @@ def is_in_US_states(state, pd_data):
         return True
     return False
 
+def answering(state,pd_data):
+    if is_in_US_states(state,pd_data):
+        if state not in answers:
+            row = pd_data[pd_data[search_col] == state].values[0]
+            t.goto(row[1],row[2])
+            t.write(state)
+
 if __name__ == '__main__':
+    # set screen
     screen = t.Screen()
-    screen.title("U.S. States Game")
+    screen.title(title_name)
+    screen.bgpic(image_path)
 
-    image = 'blank_states_img.gif'
-    screen.bgpic(image)
+    # set turtle library
+    t.hideturtle()
+    t.penup()
 
-    # input user answer
-    text_input_title = 'Guess the State'
-    text_input_prompt = "what's another state's name? "
+    # get pd_data
     data = get_state_data()
+
+    # main loop
+    # input user answer
     answer_state = screen.textinput(title=text_input_title, prompt=text_input_prompt)
-    if is_in_US_states(answer_state,data) :
-        pass
+    answering(answer_state,data)
+
+    # wait screen
     screen.mainloop()
 
 
