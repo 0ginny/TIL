97b4213ace13dc85a5b154646cbd769622995b64
Jinny Kim <kyj20214@gmail.com>
2024-05-21 11:43:40 +0900

    pandas practice

--- 100Days_of_Python_start_240515/day25_pandas/pandas_practice.py
+++ 100Days_of_Python_start_240515/day25_pandas/pandas_practice.py
@@ -4,12 +4,42 @@
 #
 # print(data)
 
-# 2. csv module 사용
-import csv
+# # 2. csv module 사용
+# import csv
+#
+# with open('weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in list(data)[1:]:
+#         temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open('weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in list(data)[1:]:
-        temperatures.append(int(row[1]))
-    print(temperatures)
+# 3. pandas 사용 : https://pandas.pydata.org/docs/
+import pandas as pd
+
+data = pd.read_csv('weather_data.csv')
+# data_dict = data.to_dict()
+# print(data_dict)
+temp_list = data['temp'].to_list()
+print(temp_list)
+temp_mean = data['temp'].mean()
+# print(temp_mean)
+temp_max = data['temp'].max()
+# print(temp_max)
+row_temp_max = data[data['temp'] == temp_max]
+# print(row_temp_max)
+
+monday = data[data['day'] == 'Monday']
+mon_temp = int(monday.temp)
+mon_f = mon_temp  * 9 / 5 + 32
+print(mon_f)
+
+# create dataframe
+data_dict = {
+    'student' :['Amy', 'John'],
+    'score' :[80,90]
+}
+data = pd.DataFrame(data_dict)
+print(data)
+# to csv
+data.to_csv('new_data.csv',index=False)
