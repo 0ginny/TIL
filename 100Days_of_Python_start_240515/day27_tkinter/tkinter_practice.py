36ba02cc182138ee18095a84911bc7cad2180056
Jinny Kim <kyj20214@gmail.com>
2024-05-21 16:27:13 +0900

    label packing practice

--- 100Days_of_Python_start_240515/day27_tkinter/tkinter_practice.py
+++ 100Days_of_Python_start_240515/day27_tkinter/tkinter_practice.py
@@ -4,4 +4,7 @@ window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
+my_label = tkinter.Label(text = 'I am a Label')
+my_label.pack() # 이게 있어야 window 안에 넣을 수 있어
+
 window.mainloop()
