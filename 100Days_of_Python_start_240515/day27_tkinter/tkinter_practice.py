6f26efa3ba52510a8870d955ad036a2a3eaf2df4
Ginny <kyj20214@gmail.com>
2024-05-22 18:28:05 +0900

    tkinter practice 2 button, entry

--- 100Days_of_Python_start_240515/day27_tkinter/tkinter_practice.py
+++ 100Days_of_Python_start_240515/day27_tkinter/tkinter_practice.py
@@ -1,10 +1,23 @@
-import tkinter
+import tkinter as tk
 
-window = tkinter.Tk()
+window = tk.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text = 'I am a Label')
+my_label = tk.Label(text = 'I am a Label')
 my_label.pack() # 이게 있어야 window 안에 넣을 수 있어
+
+def button_clicked():
+    text = input_text.get()
+    my_label['text'] = f"{text} clicked"
+    # my_label.config(text='clicked')
+
+
+button = tk.Button(text="click", command=button_clicked)
+button.pack()
+
+input_text = tk.Entry(width= 10)
+input_text.pack()
+
 
 window.mainloop()
