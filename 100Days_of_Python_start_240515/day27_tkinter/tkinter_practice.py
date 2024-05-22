2107991e57e42c4f1894912a50709a2e9d54e0b8
Ginny <kyj20214@gmail.com>
2024-05-22 19:47:55 +0900

    tkinter practice 3 place, grid

--- 100Days_of_Python_start_240515/day27_tkinter/tkinter_practice.py
+++ 100Days_of_Python_start_240515/day27_tkinter/tkinter_practice.py
@@ -5,7 +5,7 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 my_label = tk.Label(text = 'I am a Label')
-my_label.pack() # 이게 있어야 window 안에 넣을 수 있어
+my_label.grid(row = 0, column = 0) # 이게 있어야 window 안에 넣을 수 있어
 
 def button_clicked():
     text = input_text.get()
@@ -14,10 +14,18 @@ def button_clicked():
 
 
 button = tk.Button(text="click", command=button_clicked)
-button.pack()
+button.grid(row = 0, column = 5)
 
 input_text = tk.Entry(width= 10)
-input_text.pack()
+input_text.grid(row = 1, column = 1)
 
 
 window.mainloop()
+
+
+'''
+wedget 위치시키는 방법
+1. pack : 채워넣기
+2. place : 좌표로 위치시키기
+3. grid : 임의의 그리드의 행렬로 위치하기
+'''
