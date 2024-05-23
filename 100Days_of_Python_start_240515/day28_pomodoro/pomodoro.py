197d7dc59646f7a29e115267b55d81759a609ab0
Ginny <kyj20214@gmail.com>
2024-05-23 15:30:51 +0900

    make user interface

--- 100Days_of_Python_start_240515/day28_pomodoro/pomodoro.py
+++ 100Days_of_Python_start_240515/day28_pomodoro/pomodoro.py
@@ -10,9 +10,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def Timer_Reset():
+    pass
+
+
+def Timer_Start():
+    pass
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -24,10 +32,25 @@ if __name__ == '__main__':
     window.title("Pomodoro")
     window.config(padx=100, pady=100, bg=YELLOW)
 
+    # tomato canvas
     canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
     tomato_img = PhotoImage(file='tomato.png')
     canvas.create_image(100, 112, image=tomato_img)
     canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
-    canvas.pack()
+
+    # Text
+    timer_text = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, 'bold'), bg=YELLOW)
+    check_text = Label(text="✔", fg=GREEN, font=(FONT_NAME, 12, 'normal'), bg=YELLOW)
+
+    # buttons
+    start_btn = Button(text="Start", font=(FONT_NAME, 12, 'normal'), highlightthickness=0)
+    reset_btn = Button(text="Reset", font=(FONT_NAME, 12, 'normal'), highlightthickness=0)
+
+    # widget placing
+    timer_text.grid(row=1, column=2)
+    canvas.grid(row=2, column=2)
+    start_btn.grid(row=3, column=1)
+    reset_btn.grid(row=3, column=3)
+    check_text.grid(row=4, column=2)
 
     window.mainloop()
