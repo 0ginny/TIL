9a4641d6188443558b862ae3483f03c3f3446cb3
Ginny <kyj20214@gmail.com>
2024-05-23 18:53:52 +0900

    save password to txt

--- 100Days_of_Python_start_240515/day29_password_manager_tkinter/password_manager.py
+++ 100Days_of_Python_start_240515/day29_password_manager_tkinter/password_manager.py
@@ -5,15 +5,28 @@ LOGO_IMAGE_PATH = 'logo.png'
 WINDOW_PAD_X = 50
 WINDOW_PAD_Y = 50
 
-FONT = ('Arial', 12 , 'normal')
+FONT = ('Arial', 12, 'normal')
 WEB_ENTRY_WIDTH = 36
 PW_ENTRY_WIDTH = 21
 
 PW_BTN_WIDTH = 15
 ENTER_BTN_WIDTH = 36
+
+SAVE_FILE_PATH = 'data.txt'
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_to_txt():
+    web = web_ety.get()
+    email = email_ety.get()
+    pw = pw_ety.get()
+    with open(SAVE_FILE_PATH, mode='a') as file:
+        file.write(f'{web} | {email} | {pw}\n')
+        web_ety.delete(0, END)
+        pw_ety.delete(0, END)
+        web_ety.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -35,21 +48,23 @@ if __name__ == "__main__":
     # Entries
     web_ety = Entry(width=WEB_ENTRY_WIDTH, font=FONT)
     email_ety = Entry(width=WEB_ENTRY_WIDTH, font=FONT)
-    pw_ety = Entry(width=PW_ENTRY_WIDTH,font=FONT)
+    email_ety.insert(0, "kginny@gmail.com")
+    pw_ety = Entry(width=PW_ENTRY_WIDTH, font=FONT)
 
     # Buttons
     pw_btn = Button(text="Generate Password", width=PW_BTN_WIDTH, font=FONT)
-    enter_btn = Button(text="Add", width=ENTER_BTN_WIDTH, font=FONT)
+    enter_btn = Button(text="Add", width=ENTER_BTN_WIDTH, font=FONT, command=save_to_txt)
 
     # widget positioning
-    logo_canvas.grid(row = 1, column = 2)
-    web_lbl.grid(row = 2, column = 1)
-    email_lbl.grid(row = 3, column = 1)
-    password_lbl.grid(row = 4, column = 1)
-    web_ety.grid(row = 2, column = 2, columnspan = 2)
-    email_ety.grid(row = 3, column = 2, columnspan = 2)
-    pw_ety.grid(row = 4, column = 2)
-    pw_btn.grid(row = 4, column = 3)
-    enter_btn.grid(row = 5, column = 2, columnspan = 2)
+    logo_canvas.grid(row=1, column=2)
+    web_lbl.grid(row=2, column=1)
+    email_lbl.grid(row=3, column=1)
+    password_lbl.grid(row=4, column=1)
+    web_ety.grid(row=2, column=2, columnspan=2)
+    web_ety.focus()  # 커서 옮기기
+    email_ety.grid(row=3, column=2, columnspan=2)
+    pw_ety.grid(row=4, column=2)
+    pw_btn.grid(row=4, column=3)
+    enter_btn.grid(row=5, column=2, columnspan=2)
 
     window.mainloop()
