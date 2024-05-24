bd4d446915c6fe5db5b65cf750ff8cb03877d2f0
Jinny Kim <kyj20214@gmail.com>
2024-05-24 17:19:38 +0900

    day30 search data in json

--- 100Days_of_Python_start_240515/day29_password_manager_tkinter/password_manager.py
+++ 100Days_of_Python_start_240515/day29_password_manager_tkinter/password_manager.py
@@ -11,7 +11,7 @@ WINDOW_PAD_Y = 50
 
 FONT = ('Arial', 12, 'normal')
 WEB_ENTRY_WIDTH = 36
-PW_ENTRY_WIDTH = 21
+PW_ENTRY_WIDTH = 20
 
 PW_BTN_WIDTH = 15
 ENTER_BTN_WIDTH = 36
@@ -40,7 +40,27 @@ def generate_pw():
 
     pw_ety.insert(0, gen_pw)
     pyperclip.copy(gen_pw)
-
+# ---------------------------- SEARCH WEB INFO ------------------------------- #
+def search_web_info():
+    web = web_ety.get()
+    try :
+        with open(SAVE_JSON_PATH,mode = 'r') as file:
+            data = json.load(file)
+    except FileExistsError as e:
+        title = f'File Error : {e}'
+        prompt = 'No web site data exist'
+    else :
+        try :
+            email = data[web]['email']
+            pw = data[web]['password']
+        except :
+            title = f'data error'
+            prompt = f'{web} data is not exist in DB'
+        else :
+            title = web
+            prompt = f"This is the data\nemail : {email}\npassword : {pw}"
+    finally :
+        messagebox.showinfo(title=title,message=prompt)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_to_txt():
@@ -60,7 +80,7 @@ def save_to_txt():
             try:
                 with open(SAVE_JSON_PATH, mode='r') as data_file:
                     # json load
-                    data = json.load(data_file
+                    data = json.load(data_file)
             except:
                 with open(SAVE_JSON_PATH, mode='w') as data_file:
                     json.dump(obj=new_data, fp=data_file, indent=4)
@@ -95,7 +115,7 @@ if __name__ == "__main__":
     password_lbl = Label(text="Password:", font=FONT)
 
     # Entries
-    web_ety = Entry(width=WEB_ENTRY_WIDTH, font=FONT)
+    web_ety = Entry(width=PW_ENTRY_WIDTH, font=FONT)
     email_ety = Entry(width=WEB_ENTRY_WIDTH, font=FONT)
     email_ety.insert(0, "kginny@gmail.com")
     pw_ety = Entry(width=PW_ENTRY_WIDTH, font=FONT)
@@ -103,17 +123,19 @@ if __name__ == "__main__":
     # Buttons
     pw_btn = Button(text="Generate Password", width=PW_BTN_WIDTH, font=FONT, command=generate_pw)
     enter_btn = Button(text="Add", width=ENTER_BTN_WIDTH, font=FONT, command=save_to_txt)
+    search_btn = Button(text = "Search", width = PW_BTN_WIDTH, font = FONT, command = search_web_info)
 
     # widget positioning
     logo_canvas.grid(row=1, column=2)
     web_lbl.grid(row=2, column=1)
     email_lbl.grid(row=3, column=1)
     password_lbl.grid(row=4, column=1)
-    web_ety.grid(row=2, column=2, columnspan=2)
+    web_ety.grid(row=2, column=2)
     web_ety.focus()  # 커서 옮기기
     email_ety.grid(row=3, column=2, columnspan=2)
     pw_ety.grid(row=4, column=2)
     pw_btn.grid(row=4, column=3)
     enter_btn.grid(row=5, column=2, columnspan=2)
+    search_btn.grid(row = 2, column = 3)
 
     window.mainloop()
