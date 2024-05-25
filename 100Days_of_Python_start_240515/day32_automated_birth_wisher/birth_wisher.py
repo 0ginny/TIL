cf85cec93be6f08603625f0948b84bac2c6c8be8
Ginny <kyj20214@gmail.com>
2024-05-25 21:11:41 +0900

    send gmail practice

--- 100Days_of_Python_start_240515/day32_automated_birth_wisher/birth_wisher.py
+++ 100Days_of_Python_start_240515/day32_automated_birth_wisher/birth_wisher.py
@@ -1,1 +1,21 @@
 import smtplib
+
+address_path = 'address.txt'
+smtp = "smtp.gmail.com"
+port = 587
+msg = "Subject:Text\n\nThis is the test message"
+
+with open(address_path) as file :
+    text = file.readlines()
+    my_address = text[0].rstrip('\n')
+    my_pw = text[2].rstrip('\n')
+    to_address = text[3].rstrip('\n')
+
+with smtplib.SMTP(smtp,port=port) as connection :
+    connection.starttls()
+    connection.login(user=my_address,password=my_pw)
+    connection.sendmail(
+        from_addr=my_address,
+        to_addrs=to_address,
+        msg = msg
+    )
