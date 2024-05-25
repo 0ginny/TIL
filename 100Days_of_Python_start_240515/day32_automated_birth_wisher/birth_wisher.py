3db48295a49a3aa821cc7de2ec00507e067bd4a4
Ginny <kyj20214@gmail.com>
2024-05-25 23:37:07 +0900

    send quote when monday

--- 100Days_of_Python_start_240515/day32_automated_birth_wisher/birth_wisher.py
+++ 100Days_of_Python_start_240515/day32_automated_birth_wisher/birth_wisher.py
@@ -1,21 +1,35 @@
 import smtplib
+import datetime as dt
+import random as rd
 
 address_path = 'address.txt'
 smtp = "smtp.gmail.com"
 port = 587
 msg = "Subject:Text\n\nThis is the test message"
 
+weekday_for_send_quote = 5
+
+# load address info
 with open(address_path) as file :
     text = file.readlines()
     my_address = text[0].rstrip('\n')
     my_pw = text[2].rstrip('\n')
     to_address = text[3].rstrip('\n')
 
-with smtplib.SMTP(smtp,port=port) as connection :
-    connection.starttls()
-    connection.login(user=my_address,password=my_pw)
-    connection.sendmail(
-        from_addr=my_address,
-        to_addrs=to_address,
-        msg = msg
-    )
+# today weekday
+current_weekday = dt.datetime.now().weekday()
+
+with open('quotes.txt','r') as file :
+    quotes = [line.rstrip('\n') for line in file.readlines()]
+
+
+if current_weekday == weekday_for_send_quote :
+    msg = f"Subject:Today's quote\n\n{rd.choice(quotes)}"
+    with smtplib.SMTP(smtp,port=port) as connection :
+        connection.starttls()
+        connection.login(user=my_address,password=my_pw)
+        connection.sendmail(
+            from_addr=my_address,
+            to_addrs=to_address,
+            msg = msg
+        )
