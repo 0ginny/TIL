a28617252f80a16f0410990768d8a4913b90cd98
Ginny <kyj20214@gmail.com>
2024-05-26 14:35:27 +0900

    get api when need params

--- 100Days_of_Python_start_240515/day33_API/API_practice.py
+++ 100Days_of_Python_start_240515/day33_API/API_practice.py
@@ -1,12 +1,25 @@
 import requests
 
-# get data from end point
-response = requests.get(url = "http://api.open-notify.org/iss-now.json")
-# response.raise_for_status() 아래와 같이 커스터마이징도 가능
-if response.status_code == 404 :
-    raise Exception('That resource does not exist.')
-if response.status_code == 401 :
-    raise Exception('You are not authorised to access this data .')
+# # get data from end point
+# response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+# # response.raise_for_status() 아래와 같이 커스터마이징도 가능
+# if response.status_code == 404 :
+#     raise Exception('That resource does not exist.')
+# if response.status_code == 401 :
+#     raise Exception('You are not authorised to access this data .')
+#
+# data = response.json()
+# print(data)
 
-data = response.json()
-print(data)
+# 입력 파라미터가 필요할 때
+parameter = {
+    'lat' : 35.234,
+    'lng' : 140.2434,
+    'formatted' : 0
+}
+
+response = requests.get(url=" https://api.sunrise-sunset.org/json", params= parameter)
+response.raise_for_status()
+data = response.json()['results']
+for key in data:
+    print(key, data[key])
