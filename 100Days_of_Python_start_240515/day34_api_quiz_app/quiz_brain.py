9bfc4d915ba79f6705bf0672570d80aabf926eb0
Ginny <kyj20214@gmail.com>
2024-05-26 17:09:36 +0900

    change next_qustion func to return str

--- 100Days_of_Python_start_240515/day34_api_quiz_app/quiz_brain.py
+++ 100Days_of_Python_start_240515/day34_api_quiz_app/quiz_brain.py
@@ -9,11 +9,10 @@ class QuizBrain:
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
-    def next_question(self):
+    def next_question(self) -> str:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {self.current_question.text}"
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
