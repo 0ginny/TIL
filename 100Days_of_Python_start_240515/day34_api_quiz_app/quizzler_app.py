f6d6872108096ccbce7128d64c8ec86c8890abd3
Ginny <kyj20214@gmail.com>
2024-05-26 15:58:45 +0900

    change html symbols to str

--- 100Days_of_Python_start_240515/day34_api_quiz_app/quizzler_app.py
+++ 100Days_of_Python_start_240515/day34_api_quiz_app/quizzler_app.py
@@ -2,13 +2,15 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 import googletrans
+import html
 
 translater =googletrans.Translator()
 
 question_bank = []
 for question in question_data:
     question_text = question["question"]
-    question_korean = translater.translate(question_text,dest='ko').text
+    question_korean = translater.translate(question_text,dest='ko').text # 한국어로 번역
+    Encoded_q_text = html.unescape(question_korean) # 특수기호 문자열로 변경
     question_answer = question["correct_answer"]
     new_question = Question(question_korean, question_answer)
     question_bank.append(new_question)
