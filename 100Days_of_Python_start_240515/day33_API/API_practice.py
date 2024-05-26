d6b5761e013c585da51887d7a5ec1125a3827e2c
Ginny <kyj20214@gmail.com>
2024-05-26 13:47:19 +0900

    response error process

---
+++ 100Days_of_Python_start_240515/day33_API/API_practice.py
@@ -0,0 +1,9 @@
+import requests
+
+# get data from end point
+response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+# response.raise_for_status() 아래와 같이 커스터마이징도 가능
+if response.status_code == 404 :
+    raise Exception('That resource does not exist.')
+if response.status_code == 401 :
+    raise Exception('You are not authorised to access this data .')
