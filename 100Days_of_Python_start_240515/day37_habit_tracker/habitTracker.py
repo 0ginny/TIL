aae9432f04529176ffcc06c2fd94324817348a40
Ginny <kyj20214@gmail.com>
2024-05-27 22:34:47 +0900

    create user graph by pixela

--- 100Days_of_Python_start_240515/day37_habit_tracker/habitTracker.py
+++ 100Days_of_Python_start_240515/day37_habit_tracker/habitTracker.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+# api doc : https://docs.pixe.la/
+
 # 1. create user
 pixela_url = "https://pixe.la/v1/users"
 
@@ -14,5 +16,25 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_url,json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_url,json=user_params)
+
+# 2. create graph
+graph_endpoint = f"{pixela_url}/{data['username']}/graphs"
+
+graph_config = {
+    "id" : data["graph_id"],
+    "name" : "Studying Graph",
+    "unit" : "min",
+    "type" : "int",
+    "color" : "sora"
+}
+
+headers = {
+    "X-USER-TOKEN":data["token"]
+}
+
+# response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+
+# 생성된 주소
+print(f'{graph_endpoint}/{graph_config["id"]}.html')
