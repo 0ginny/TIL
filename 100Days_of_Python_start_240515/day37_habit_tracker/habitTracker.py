fc4763825622866510748537341d0ccc04aaf284
Ginny <kyj20214@gmail.com>
2024-05-27 22:19:12 +0900

    create pixela site

--- 100Days_of_Python_start_240515/day37_habit_tracker/habitTracker.py
+++ 100Days_of_Python_start_240515/day37_habit_tracker/habitTracker.py
@@ -1,14 +1,18 @@
 import requests
-
+import json
 
 # 1. create user
 pixela_url = "https://pixe.la/v1/users"
 
+with open('secret.json') as file:
+    data = json.load(file)
 
-user_params ={
-    "token" : "",# 임의로 정하는 거야ㅑ.
-    "username" : ""
+user_params = {
+    "token": data['token'],  # 임의로 정하는 거야ㅑ.
+    "username": data['username'],
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
 }
-"""
 
-"""
+response = requests.post(url=pixela_url,json=user_params)
+print(response.text)
