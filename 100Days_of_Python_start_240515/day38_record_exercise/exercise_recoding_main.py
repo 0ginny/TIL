c051107bf597c4ec8d01181dc4a305306ea8d889
Ginny <kyj20214@gmail.com>
2024-05-29 18:14:50 +0900

    post google sheet practice

--- 100Days_of_Python_start_240515/day38_record_exercise/exercise_recoding_main.py
+++ 100Days_of_Python_start_240515/day38_record_exercise/exercise_recoding_main.py
@@ -1,16 +1,20 @@
 import json
 import requests as rq
+import datetime as dt
 
 MY_WEIGHT = 75
 MY_HEIGHT = 175.6
 MY_AGE = 27
 
-with open('record_exercise_secret.json') as file :
+with open('record_exercise_secret.json') as file:
     secret_data = json.load(file)
-    NUTRITION_ID = secret_data['nutrition_id']
-    NUTRITION_KEY = secret_data['nutrition_key']
-
-
+NUTRITION_ID = secret_data['nutrition_id']
+NUTRITION_KEY = secret_data['nutrition_key']
+sheety_userCode = secret_data['sheety_userCode']
+sheety_projectName = secret_data['sheety_projectName']
+sheety_sheetName = secret_data['sheety_sheetName']
+sheety_name = secret_data['sheety_name']
+sheety_email = secret_data['sheety_email']
 
 # 1. nutrition data 불러오기
 
@@ -18,20 +22,39 @@ NUTRITION_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
 # Nutritional api doc : https://docx.syndigo.com/developers/docs/natural-language-for-exercise
 
-HEADER= {
+HEADER = {
     'Content-Type': 'application/json',
     'x-app-id': NUTRITION_ID,
     'x-app-key': NUTRITION_KEY
-  }
-
-CONFIG = {
-    'query' : input("Tell me which exercises you did : "),
-    'weight_kg':MY_WEIGHT,
-    'height_cm':MY_HEIGHT,
-    'age':MY_AGE
 }
 
-response = rq.post(url=NUTRITION_ENDPOINT,json=CONFIG,headers=HEADER)
-print(response.text)
+CONFIG = {
+    'query': "running for 2 hour",  # input("Tell me which exercises you did : ")
+    'weight_kg': MY_WEIGHT,
+    'height_cm': MY_HEIGHT,
+    'age': MY_AGE
+}
 
+# response = rq.post(url=NUTRITION_ENDPOINT, json=CONFIG, headers=HEADER)
+# print(response.text)
 
+# 2. sheety 로 google sheet 수정하기
+# sheety url : https://dashboard.sheety.co/login
+SHEETY_ENDPOINT = f"https://api.sheety.co/{sheety_userCode}/{sheety_projectName}/{sheety_sheetName}"
+print(SHEETY_ENDPOINT)
+
+date = dt.datetime.now().strftime("%d/%m/%Y")
+
+# "workouts" 에서 s를 빼야해. 자연어 인식인가? 중복이 아닌 글자로 해야해.
+SHEETY_JSON = {
+    "workout": {
+        "date": date,
+        "time": "15:00:00",
+        "exercise": "Running",
+        "duration": "22",
+        "calories": "130"
+    }
+}
+
+rq_sheety_get = rq.post(url=SHEETY_ENDPOINT, json=SHEETY_JSON)
+print(rq_sheety_get.text)
