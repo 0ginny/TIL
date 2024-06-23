35b80c17bd8e3aedfa512357e776de986116dfb7
Ginny <kyj20214@gmail.com>
2024-06-23 19:28:39 +0900

    find element

--- 100Days_of_Python_start_240515/day48_Selenium/selenium_practice_main.py
+++ 100Days_of_Python_start_240515/day48_Selenium/selenium_practice_main.py
@@ -8,6 +8,7 @@ chrome_options.add_experimental_option("detach",True)
 # headless option (창을 띄우지 않음) - 보안이 우회가능
 chrome_options.add_argument("--headless")
 
+
 # driver connection
 goto_url = "https://www.amazon.com/Seagate-Expansion-Desktop-Drive-Black/dp/B07VS8QCXC/ref=sr_1_1?crid=1YXA3ADINFS8O&dib=eyJ2IjoiMSJ9.O-iz_OxFgbx8bLQuv31KFNq9EqEsv06BvQkuwEn6cNPNtFGUdYqwHmnbzkunELII3GzVPoGIDiQUnkTCGdDqjQly1Fhy8RbRdvxYRjrrcAG4n8hLu605tUVwzGp_Ft1N9Nz7RQqs26WYAQsuENh_GXyxzssO78dshrh_jodARrBN_Dp8LGOVPPKvhLVyFw7tui9ggRl03cAFSKPv2iVV-MXw-Z3-lIMBG3QXB21Khso.I3JpZThvqbKGyQYrEMFHxSSXuMTPDdCwL__aAMPv4Mo&dib_tag=se&keywords=portable%2Bhard%2Bdisk%2B8tb&qid=1719035880&sprefix=portablehard%2Bdisk%2B8tb%2Caps%2C221&sr=8-1&th=1"
 driver = webdriver.Chrome(options=chrome_options)
@@ -15,6 +16,7 @@ driver.get(goto_url)
 
 price_dollar = driver.find_element(By.CLASS_NAME, value='a-price-whole')
 price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
+# 가장 편한건 By.XPATH 를 쓰면 그냥 복사 붙여넣기 하면 돼.
 
 print(f"The price is {price_dollar.text}.{price_cents.text}")
 # close just chrome tap
