047f2df38ec80cdc09c7763b6cd9fb82c74fdf87
Jinny Kim <kyj20214@gmail.com>
2024-06-30 23:02:38 +0900

    setting envirment valiable

--- 100Days_of_Python_start_240515/day54_flask/flask_practice.py
+++ 100Days_of_Python_start_240515/day54_flask/flask_practice.py
@@ -8,3 +8,6 @@ def hello_world():
     return 'Hello, World!'
 
 # window는 set으로 해야해.
+# terminal 에 실행 (Command Prompt 로 실행)
+# cd 100Days_of_Python_start_240515/day54_flask
+# set FLASK_APP=flask_practice.py
