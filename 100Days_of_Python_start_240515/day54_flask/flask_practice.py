3c21a9b5f651b10f5a0f626ee5c5e049ade8d70a
Jinny Kim <kyj20214@gmail.com>
2024-06-30 23:58:02 +0900

    __name__ 설명

--- 100Days_of_Python_start_240515/day54_flask/flask_practice.py
+++ 100Days_of_Python_start_240515/day54_flask/flask_practice.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# __name__의 의미 : 작동되는 파일? 같은 느낌?
+print(__name__) # __main__
+print(Flask.__name__)
 
 @app.route('/')
 def hello_world():
@@ -11,3 +14,7 @@ def hello_world():
 # terminal 에 실행 (Command Prompt 로 실행)
 # cd 100Days_of_Python_start_240515/day54_flask
 # set FLASK_APP=flask_practice.py
+
+# 두번째 방법
+# if __name__ == "__main__":
+#     app.run()
