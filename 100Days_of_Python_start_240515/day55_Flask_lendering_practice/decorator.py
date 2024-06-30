617951d9007c98924142643979925c5666b0ad1e
Jinny Kim <kyj20214@gmail.com>
2024-07-01 01:22:12 +0900

    고급 데코레이터 연습

--- 100Days_of_Python_start_240515/day55_Flask_lendering_practice/decorator.py
+++ 100Days_of_Python_start_240515/day55_Flask_lendering_practice/decorator.py
@@ -17,12 +17,43 @@ def make_underlined(func):
         return f'<u>{func()}</u>'
     return decorated_func
 
+@app.route('/')
+def home():
+    return "Hello World"
+
 @app.route('/hello')
 @make_bold
 @make_underlined
 @make_emphasis
 def hello():
     return "hello"
+
+# arg kwarg 이용
+class User:
+    def __init__(self,name):
+        self.name = name
+        self.logged = False
+
+def is_authenticated(func):
+    def wrapper(*args,**kwargs):
+        if args[0].logged == True:
+            func(args[0])
+    return wrapper
+
+
+@app.route('/set/<name>')
+def setting(name):
+    global user
+    user = User(name)
+    user.logged = True
+
+
+@app.route('/post')
+@is_authenticated
+def post(user):
+    return f"This is {user.name}'s new blog post"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
