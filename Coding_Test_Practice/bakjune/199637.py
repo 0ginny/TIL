fc9be4eaa27fb6836b1f66fd3cbd37313cd04581
Jinny Kim <kyj20214@gmail.com>
2024-08-09 10:34:16 +0900

    runtime error index

--- Coding_Test_Practice/bakjune/199637.py
+++ Coding_Test_Practice/bakjune/199637.py
@@ -7,11 +7,20 @@
 입력 :
 첫 번째 줄에는 칭호의 개수 N (1 ≤ N ≤ 10^5)과 칭호를 출력해야 하는 캐릭터들의 개수 M (1 ≤ M ≤ 10^5)이 빈칸을 사이에 두고 주어진다. (1 ≤ N, M ≤ 105)
 칭호는 전투력 상한값의 비내림차순으로 주어진다.
-
+해당하는 칭호가 없는 전투력은 입력으로 주어지지 않는다.
+    - 일단 전투력의 다른 제약조건은 없어. 중복은 가능한가봐, sort도 안되있을 거고
 
 출력 :
 
 출력할 수 있는 칭호가 여러 개인 경우 가장 먼저 입력된 칭호 하나만 출력한다.
+칭호는 비 내림 차순이야.
+score 가 같으면 먼저 나온 거
+100 a
+101 b
+102 c
+
+101 이면 b 잖아.
+
 
 1. 아이디어
 구현 :
@@ -20,6 +29,7 @@
 
 --------------
 모두 list 에 넣고 sort : 10^5 * ln10^5
+sort 를 하더라도 이전 순서대로 출력을 해야해.
 그후 idx +=  1 하면서 비교 : 10^5
 가능
 '''
@@ -40,17 +50,20 @@ for _ in range(N): #10^5
     except KeyError:
         label_dict[score] = label
 
-case = [int(input()) for _ in range(M)]
-case.sort()
+raw_case = [int(input()) for _ in range(M)]
+# sort 전 index 저장
 
-ans = []
+case = sorted(raw_case)
+
+ans = {}
 cnt = 0
 for (s,l) in label_dict.items():
     while case[cnt] <= s:
-        ans.append(l)
+        ans[case[cnt]] = l
         cnt+=1
         if cnt == M:
             break
+for c in raw_case:
+    print(ans[c])
 
-for a in ans:
-    print(a)
+# runtime error(index)
