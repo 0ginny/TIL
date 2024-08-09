41a839516be101a3ac612e450df22b08a0d23ad0
Jinny Kim <kyj20214@gmail.com>
2024-08-09 10:24:31 +0900

    sort idx 비교 실패.. why?

--- Coding_Test_Practice/bakjune/199637.py
+++ Coding_Test_Practice/bakjune/199637.py
@@ -5,23 +5,33 @@
 전투력 10,000 이하의 캐릭터는 WEAK, 10,000 초과 그리고 100,000 이하의 캐릭터는 NORMAL, 100,000 초과 그리고 1,000,000 이하의 캐릭터는 STRONG 칭호를 붙여준다
 
 입력 :
-첫 번째 줄에는 칭호의 개수 N (1 ≤ N ≤ 105)과 칭호를 출력해야 하는 캐릭터들의 개수 M (1 ≤ M ≤ 105)이 빈칸을 사이에 두고 주어진다. (1 ≤ N, M ≤ 105)
+첫 번째 줄에는 칭호의 개수 N (1 ≤ N ≤ 10^5)과 칭호를 출력해야 하는 캐릭터들의 개수 M (1 ≤ M ≤ 10^5)이 빈칸을 사이에 두고 주어진다. (1 ≤ N, M ≤ 105)
 칭호는 전투력 상한값의 비내림차순으로 주어진다.
 
 
 출력 :
 
 출력할 수 있는 칭호가 여러 개인 경우 가장 먼저 입력된 칭호 하나만 출력한다.
+
+1. 아이디어
+구현 :
+    for 문 if 문
+시간복잡도 실패 10 ^10
+
+--------------
+모두 list 에 넣고 sort : 10^5 * ln10^5
+그후 idx +=  1 하면서 비교 : 10^5
+가능
 '''
 
 import sys
 
 input = sys.stdin.readline
 
-N, case = map(int, input().split())
+N, M = map(int, input().split())
 
 label_dict = {}
-for _ in range(N):
+for _ in range(N): #10^5
     label, score = input().rstrip().split()
     score = int(score)
     try :
@@ -30,16 +40,17 @@ for _ in range(N):
     except KeyError:
         label_dict[score] = label
 
-def chk(n) -> str:
-    for (s, l) in label_dict.items():
-        if n <= s:
-            return l
-    return "불가능한 값"
+case = [int(input()) for _ in range(M)]
+case.sort()
 
 ans = []
-for _ in range(case):
-    num = int(input())
-    ans.append(chk(num))
+cnt = 0
+for (s,l) in label_dict.items():
+    while case[cnt] <= s:
+        ans.append(l)
+        cnt+=1
+        if cnt == M:
+            break
 
 for a in ans:
     print(a)
