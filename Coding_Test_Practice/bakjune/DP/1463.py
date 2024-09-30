b12ec060302b377c8ebba5f0c7afbb5df2996df7
Ginny <kyj20214@gmail.com>
2024-09-30 20:20:42 +0900

    다시 풀어야 해서 화면 초기화

--- Coding_Test_Practice/bakjune/DP/1463.py
+++ Coding_Test_Practice/bakjune/DP/1463.py
@@ -1,30 +1,3 @@
 '''
-시작 : 19:00
-
-
-백트래킹 처럼 하면 될 듯
-시간 초과
-
-DP 에서 (n-1 /3 ,n/2)
+다시 풀어야 해서 삭제
 '''
-
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-
-dp = [0] * (N+1)
-
-for i in range(2, N + 1):
-    # 1을 뺀 경우
-    dp[i] = dp[i - 1] + 1
-
-    # 2로 나누어지는 경우
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
-
-    # 3으로 나누어지는 경우
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
-
-print(dp)
