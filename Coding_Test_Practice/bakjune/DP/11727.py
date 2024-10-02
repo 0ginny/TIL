aaea56adf28009733b5774c38ecd9cba44cab7ed
Ginny <kyj20214@gmail.com>
2024-10-02 19:39:18 +0900

    실버3 dp 30분 경과, 쉬운데 규칙 찾는 게 어려웠어. 더 연습해야할 듯

---
+++ Coding_Test_Practice/bakjune/DP/11727.py
@@ -0,0 +1,20 @@
+'''
+시작: 19:07
+끝 : 19:37
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * 1001
+
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1] + dp[i-2] * 2
+
+print(dp[n]%10007)
