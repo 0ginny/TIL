53f363ee67b207c27cc85f49df3c5ec1e910c326
Ginny <kyj20214@gmail.com>
2024-10-03 16:37:01 +0900

    골드3 실패,, dfs 와 dp를 같이 써서 풀어야 했어. 그러니까 dp에서 문제가 되는 부분에서만 dfs 를 써야 했던 거지

--- Coding_Test_Practice/bakjune/DP/1520.py
+++ Coding_Test_Practice/bakjune/DP/1520.py
@@ -5,6 +5,12 @@
 
 dfs 보다 dp 로 하는 게 더 편할 듯, 왜냐하면 어차피 위에서 아래로만 갈 수 있어서 뒤로 가는 경우는 없어.
 근데 그냥 전체 탐색으로 하면 뒤로 갈 때 더하는 순서가 달라서 문제가 틀려
+dfs 로 해야하나봐- 시간 초과 안나올까? 일단 뒤로 갈 수는 없어. 모든 경우를 다 하는 거긴 해, N*M 가능할듯
+아니네,, 시간초과네...
+
+dp 의 특성을 이용해야해. 이단 작은 그룹부터 어떻게 할지 생각해
+
+
 
 '''
 
@@ -21,7 +27,6 @@ dp = [[0] * W for _ in range(H)]
 di = [0, 1, 0, -1]
 dj = [1, 0, -1, 0]
 dp[0][0] = 1
-q = [(0,0)]
 
 for i in range(H):
     for j in range(W):
@@ -29,8 +34,8 @@ for i in range(H):
             ni = i + di[k]
             nj = j + dj[k]
             if 0 <= ni < H and 0 <= nj < W :
-                print(f' ni: {ni}, nj : {nj}, m[ni][nj] {m[ni][nj]}, m[i][j] : {m[i][j]}')
-                if m[ni][nj] < m[i][j]:
-                    dp[ni][nj] += dp[i][j]
+                # print(f' ni: {ni}, nj : {nj}, m[ni][nj] {m[ni][nj]}, m[i][j] : {m[i][j]}')
+                if m[ni][nj] > m[i][j]:
+                    dp[i][j] += dp[ni][nj]
 print(dp)
 
