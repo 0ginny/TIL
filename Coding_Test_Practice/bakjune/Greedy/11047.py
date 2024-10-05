89c29e88ced5d1fd05db7356efb9a205804df419
Ginny <kyj20214@gmail.com>
2024-10-05 12:46:38 +0900

    실버4 그리디 시간초과

---
+++ Coding_Test_Practice/bakjune/Greedy/11047.py
@@ -0,0 +1,23 @@
+'''
+시작 : 12:40
+
+오름차순으로 주어지기에, 아래부터 동전을 쓰고 안되면 위로 가면 돼.
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+N,K = map(int,input().split())
+
+coins = [int(input()) for _ in range(N)]
+
+ans = 0
+for c in coins[::-1]:
+    while K >= c :
+        ans += 1
+        K -= c
+    if K == 0 :
+        break
+print(ans)
+
